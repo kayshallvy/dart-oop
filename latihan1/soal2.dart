c873aad8afa130a9366785a67445da60941c902c
kayshallvy <kayzaalvy1214@gmail.com>
2026-03-28 15:10:22 +0700

    Membuat Class Mahasiswa

---
+++ latihan1/soal2.dart
@@ -0,0 +1,12 @@
+class Mahasiswa {
+  String nama = "Kaysha";
+  String nim = "231240001359";
+}
+
+void main() {
+  Mahasiswa mhs1 = Mahasiswa();
+
+  print("Informasi Mahasiswa:");
+  print("Nama: ${mhs1.nama}");
+  print("NIM: ${mhs1.nim}");
+}
